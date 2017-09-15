6b4f91e806f6368f9034326c928e401c35eb6c85
David Laidlaw <davidlaidlaw15@gmail.com>
2017-09-14 20:22:48 -0700

    Updated server.js file with heroku PORT listener.

--- server.js
+++ server.js
@@ -357,6 +357,6 @@ app.get("/delete/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
